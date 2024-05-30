d9b9c4201a9d18bac075626701c91c232734880e
LyPahn <91794300+LyPahn@users.noreply.github.com>
2024-05-30 15:08:29 +0700

    30/5

--- src/main/java/com/bkap/services/CartService.java
+++
@@ -1,8 +0,0 @@
-package com.bkap.services;
-
-import com.bkap.entities.Cart;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CartService extends GenericService<Cart , Integer> {
-}
