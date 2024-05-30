c7ff19186cfd781613abd7cf0e0a23af03f627a3
LyPahn <91794300+LyPahn@users.noreply.github.com>
2024-05-30 17:02:17 +0700

    Revert "30/5"
    This reverts commit c4eaa732bc2cc6eb4641540c06e921a690487e03.

---
+++ src/main/java/com/bkap/entities/User.java
@@ -0,0 +1,51 @@
+package com.bkap.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "firstName" , columnDefinition = "NVARCHAR(50)")
+    private String firstName;
+    @Column(name = "lastName" , columnDefinition = "NVARCHAR(50)")
+    private String lastName;
+    @Column(name = "address" , columnDefinition = "NVARCHAR(255)")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "status")
+    private boolean status;
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Temporal(TemporalType.DATE)
+    private Date created_at;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "role")
+    private boolean role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY , cascade = CascadeType.ALL)
+    private List<Cart> carts;
+}
